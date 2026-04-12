1bf9791a001f4ae170f4d00dfbce703d9f3c55f9
kauecamelo-br <ewerton.ecc@gmail.com>
2026-04-12 13:25:11 -0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -9,12 +9,4 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  server: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
-  preview: {
-    host: "0.0.0.0",
-    port: 5173,
-  },
 });
